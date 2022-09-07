5dfef8c7168b2d957747cff2b0cba98e575b588b
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-09-07 09:53:54 +0900

    3_12_set_hilt_app_and_entry_point

--- app/src/main/java/com/example/jettodoapp/MainActivity.kt
+++ app/src/main/java/com/example/jettodoapp/MainActivity.kt
@@ -6,12 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.jettodoapp.ui.theme.JetTodoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
